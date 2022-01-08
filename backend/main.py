dd393b7a2b5f8123c59011bef7f0127ced9d38e4
Junaid8239 <mj9945801@gmail.com>
2022-01-08 23:32:26 +0530

    uploaded cofee

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask,redirect,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 #mydatabase connection
 local_server=True
@@ -9,8 +10,29 @@ app.secret_key="madhujunaid"
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-   app.run(debug = True)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/autoease'
+db=SQLAlchemy(app)
+
+class Test(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50))
+
+
+
+@app.route("/test")
+def test():
+    try:
+        a=Test.query.all()
+        print(a)
+        return('connected')
+    except Exception as e:
+        print(e)
+        return f'not{e}'   
+
+
+
+
+app.run(debug = True)
 
 
 
